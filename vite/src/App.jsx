9f656abc1ae5cdf437dc9ab7a465d796d310f68b
h662 <h662hong@gmail.com>
2024-05-24 13:26:13 +0900

    Balance ing...

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,12 +1,27 @@
 import { useEffect, useState } from "react";
 import MetamaskButton from "./components/MetamaskButton";
 import Erc20Connect from "./components/Erc20Connect";
+import { formatEther } from "ethers";
 
 const App = () => {
   const [signer, setSigner] = useState();
   const [contract, setContract] = useState();
   const [name, setName] = useState();
   const [symbol, setSymbol] = useState();
+  const [balanceAddress, setBalanceAddress] = useState("");
+  const [balance, setBalance] = useState();
+
+  const onClickBalance = async () => {
+    try {
+      if (!balanceAddress) return;
+
+      const response = await contract.balanceOf(balanceAddress);
+
+      setBalance(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const getNameSymbol = async () => {
     try {
@@ -34,12 +49,36 @@ const App = () => {
           <div className="box-style text-center">
             0x77D2DAC005A952eF61AbC3D5b460bF60c805E790
           </div>
-          <Erc20Connect
-            name={name}
-            symbol={symbol}
-            signer={signer}
-            setContract={setContract}
-          />
+          <Erc20Connect name={name} signer={signer} setContract={setContract} />
+          {name && (
+            <div className="flex w-full items-start">
+              <div className="flex flex-col gap-2 grow">
+                <div className="ml-1 text-lg font-bold">
+                  {name}
+                  <span className="font-normal">({symbol})</span> 토큰 확인
+                </div>
+                {balance ? (
+                  <div className="box-style">
+                    {formatEther(balance)} {symbol}
+                  </div>
+                ) : (
+                  <input
+                    className="input-style"
+                    type="text"
+                    placeholder="지갑 주소"
+                    value={balanceAddress}
+                    onChange={(e) => setBalanceAddress(e.target.value)}
+                  />
+                )}
+              </div>
+              <button
+                className="button-style ml-4 mt-9"
+                onClick={onClickBalance}
+              >
+                확인
+              </button>
+            </div>
+          )}
         </div>
       )}
     </div>
